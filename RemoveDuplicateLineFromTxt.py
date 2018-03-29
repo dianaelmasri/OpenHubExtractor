6f6c3a30c71b8a6d8b507ed327afcfc5d4e8aced
dianaelmasri <dianaelmasri3@gmail.com>
2018-03-29 13:26:08 -0400

    Add files via upload
    Script to remove duplicate in file

---
+++ RemoveDuplicateLineFromTxt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+with open(inputFile) as result:
+    uniqlines = set(result.readlines())
+    with open(outputFile, 'w') as rmdup:
+        rmdup.writelines(set(uniqlines))
